638af761db1fde1e626dba086273fa10f59390e4
samrespass <samrespass@gmail.com>
2022-07-05 08:54:37 -0400

    whoops

--- djamming/models.py
+++ djamming/models.py
@@ -4,7 +4,7 @@ class Track(models.Model):
     song = models.CharField(max_length=255, null=False, blank=False)
     def __str__(self):
         return(self.song)
-    album = models.ForeignKey( "Album", on_delete=models.CASCADE, related_name="album_tracks", blank=False, null=False )
+    album = models.ForeignKey( "Album", on_delete=models.CASCADE, related_name="tracks", blank=False, null=False )
 
 class Album(models.Model):
     artist = models.CharField(max_length=255, null=False, blank=False)
@@ -13,7 +13,6 @@ class Album(models.Model):
         return(self.name)
     created = models.DateField(blank=True,null=True, auto_now_add=True)
     album_art = models.ImageField(upload_to='static/images/album_art/')
-    tracks = models.ForeignKey( Track, on_delete=models.CASCADE, related_name="album_tracks", blank=True, null=True )
 
 
 class Artist(models.Model):
